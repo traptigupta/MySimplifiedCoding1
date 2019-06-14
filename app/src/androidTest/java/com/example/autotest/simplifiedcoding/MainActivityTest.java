1f372ac28759638b407e6f3a68f3ab7b56d9f2ff
traptigupta <trapti.gupta968@gmail.com>
2019-06-14 17:51:02 +0530

    Unit Test2

--- app/src/androidTest/java/com/example/autotest/simplifiedcoding/MainActivityTest.java
+++ app/src/androidTest/java/com/example/autotest/simplifiedcoding/MainActivityTest.java
@@ -15,6 +15,7 @@ import org.junit.Test;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
@@ -25,26 +26,47 @@ public class MainActivityTest {
     private MainActivity mActivity=null;
     public Instrumentation instr= InstrumentationRegistry.getInstrumentation();
 
-     Instrumentation.ActivityMonitor monitor= instr.addMonitor(LoginActivity.class.getName(),null,false);
+     Instrumentation.ActivityMonitor monitor= instr.addMonitor(ProfileActivity.class.getName(),null,false);
     @Before
     public void setUp() throws Exception {
         mActivity=mActivityTestRule.getActivity();
     }
     @Test
     public void testLaunch() {
-        String name="admin";
-        String password="12341";
-        Espresso.onView(withId(R.id.textViewLogin)).perform(click());
+        //String name="admin";
+        //String password="12341";
+        //Espresso.onView(withId(R.id.textViewLogin)).perform(click());
 //        Activity secondActivity= instr.waitForMonitorWithTimeout(monitor,5000);
   //      assertNotNull(secondActivity);
-        Espresso.onView(withId(R.id.editTextUsername)).perform(typeText(name));
+        /*Espresso.onView(withId(R.id.editTextUsername)).perform(typeText(name));
         Espresso.onView(withId(R.id.editTextPassword)).perform(typeText(password));
         Espresso.closeSoftKeyboard();
         Espresso.onView(withId(R.id.buttonLogin)).perform(click());
         Espresso.onView(withId(R.id.textViewUsername)).check(matches(withText(name)));
-        Espresso.onView(withId(R.id.buttonLogout)).perform(click());
+        Espresso.onView(withId(R.id.buttonLogout)).perform(click());*/
 
-       // textViewUsername
+        Espresso.onView(withId(R.id.buttonRegister)).perform(click());
+        Espresso.onView(withId(R.id.editTextUsername)).check(matches(hasErrorText("Please enter username")));
+        Espresso.onView(withId(R.id.editTextUsername)).perform(typeText("test"));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(withId(R.id.buttonRegister)).perform(click());
+        Espresso.onView(withId(R.id.editTextEmail)).check(matches(hasErrorText("Please enter your email")));
+        Espresso.onView(withId(R.id.editTextEmail)).perform(typeText("test"));
+        Espresso.closeSoftKeyboard();
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(withId(R.id.buttonRegister)).perform(click());
+        Espresso.onView(withId(R.id.editTextEmail)).check(matches(hasErrorText("Enter a valid email")));
+        Espresso.onView(withId(R.id.editTextEmail)).perform(typeText("@gmail.com"));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(withId(R.id.buttonRegister)).perform(click());
+        Espresso.onView(withId(R.id.editTextPassword1)).check(matches(hasErrorText("Enter a password")));
+        Espresso.onView(withId(R.id.editTextPassword1)).perform(typeText("12341"));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(withId(R.id.buttonRegister)).perform(click());
+        Activity secondActivity= instr.waitForMonitorWithTimeout(monitor,5000);
+          assertNotNull(secondActivity);
+
+
 
     }
     @After
